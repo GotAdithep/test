3e1242518e76182eaa80a415b0ad74a4f0ee7b17
Adithep <adithep.su@ku.th>
2025-04-15 13:28:33 +0700

    test push

--- hi.py
+++ hi.py
@@ -1,7 +1,1 @@
-list = []
-number = int(input(f"input "))
-if 1 <= number and number <= 10000:
-    for i in range(1,number+1):
-        list.append(i)
-
-print(f"output {sum(list)}")
+print('oihfg')
